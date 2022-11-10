d15b8b34515d2c26aa590f95924b491e4f50b05f
apple <apple@apples-MacBook-Air.local>
2022-11-10 14:15:16 -0600

    First File

---
+++ program1.py Original.py
@@ -0,0 +1,20 @@
+
+print('Hello World')
+
+print('What is your name?  ')
+
+name = input()
+
+print('Good Morning', name)
+
+print('What time is it?  ')
+
+time = input()
+
+print('The time is', time)
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+# This is a comment
+
